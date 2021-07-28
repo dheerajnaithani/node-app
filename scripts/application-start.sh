c2f74738ad37a046b2bb5151be8d078fbf0ca522
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-07-28 13:29:13 -0400

    Add env to deployment scripts

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -2,4 +2,4 @@ set -e
 mkdir -p /opt/xeniuser/node-app
 cd /opt/xeniuser/node-app
 
-/usr/local/bin/pm2 start src/ecosystem.config.js --env __DEPLOYMENT_ENV_NAME__
+/usr/local/bin/pm2 start src/ecosystem.config.js --env #${DEPLOYMENT_ENV_NAME}#
