3a774fb1c10bcc7ddac0510de60b71b25880e75c
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-07-29 22:18:31 -0400

    make calls to aws services synchronous

--- src/app/utils/parameters.js
+++ src/app/utils/parameters.js
@@ -2,14 +2,13 @@ const AWS = require('aws-sdk')
 
 const fetchParameterValue = (region, parameterName) => {
   const client = new AWS.SSM({ region })
-  const result = client.getParameter({ Name: parameterName }, (err, data) => {
+  return client.getParameter({ Name: parameterName }, (err, data) => {
     if (err) {
       throw err
     } else {
       return data.Parameter.Value
     }
   })
-  return result
 }
 
 module.exports = fetchParameterValue
--- src/app/utils/secrets.js
+++ src/app/utils/secrets.js
@@ -2,17 +2,13 @@ const AWS = require('aws-sdk')
 
 function fetchSecret(region, secretName) {
   const client = new AWS.SecretsManager({ region })
-  const result = client.getSecretValue(
-    { SecretId: secretName },
-    (err, data) => {
-      if (err) {
-        throw err
-      } else {
-        return data.SecretString
-      }
-    },
-  )
-  return result
+  return client.getSecretValue({ SecretId: secretName }, (err, data) => {
+    if (err) {
+      throw err
+    } else {
+      return data.SecretString
+    }
+  })
 }
 
 module.exports = fetchSecret
